5096a25fdf698930ce9bd64bc666977bdc53f41a
Pvblo76 <verdurapaolo920@gmail.com>
2025-10-25 20:28:35 +0200

    Add VideoModel class for video data representation

---
+++ lib/screens/models/video_model.dart
@@ -0,0 +1,8 @@
+class VideoModel {
+  final String id;
+  final String title;
+  final String url;
+  final String thumbnail;
+
+  VideoModel({required this.id, required this.title, required this.url, required this.thumbnail});
+}
